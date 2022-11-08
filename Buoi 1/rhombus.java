9fe76e2bf4c40678358d33f51f86fb94af9042ce
nghiaabh <110017168+bhnghiaa@users.noreply.github.com>
2022-11-08 15:41:50 +0700

    Create rhombus.java

---
+++ Buoi 1/rhombus.java
@@ -0,0 +1,15 @@
+package cigg;
+
+public class rhombus {
+    public float a;
+    public float d1;
+    public float d2;
+    public float perimeter()
+    {
+        return a*4;
+    }
+    public float area()
+    {
+        return (float)(d1*d2) / 2;
+    }
+}
