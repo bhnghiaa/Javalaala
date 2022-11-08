e3208f5c0671f222cbf6ce0a71cfbaa3393cf270
nghiaabh <110017168+bhnghiaa@users.noreply.github.com>
2022-11-08 15:41:53 +0700

    Create square.java

---
+++ Buoi 1/square.java
@@ -0,0 +1,13 @@
+package cigg;
+
+public class square {
+    public float x;
+    public float perimeter()
+    {
+        return x*4;
+    }
+    public float area()
+    {
+        return x * x;
+    }
+}
