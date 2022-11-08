a6eee9ce542958cdcd2394d0641f687684567b6c
nghiaabh <110017168+bhnghiaa@users.noreply.github.com>
2022-11-08 15:41:47 +0700

    Create rectangular.java

---
+++ Buoi 1/rectangular.java
@@ -0,0 +1,14 @@
+package cigg;
+
+public class rectangular {
+    public float a;
+    public float b;
+    public float perimeter()
+    {
+        return 2*(a+b);
+    }
+    public float area()
+    {
+        return a*b;
+    }
+}
