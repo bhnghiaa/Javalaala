f6df2576988802f56172b153e59b22f86c681d37
nghiaabh <110017168+bhnghiaa@users.noreply.github.com>
2022-11-15 19:45:34 +0700

    Create Brand.java

---
+++ Buoi5/Brand.java
@@ -0,0 +1,21 @@
+package Buoi5;
+
+import java.util.Scanner;
+
+public class Brand {
+    private String name;
+    public static Scanner scanner = new Scanner(System.in);
+
+    public Brand(String name) {
+
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
